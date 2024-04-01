0fd3169447e0a91064a3dc5455785675fa43ebea
GodwinCyber <godwinchuks032@gmail.com>
2024-03-31 19:58:23 -0700

    create a new view for City objects that handles all default RESTFul API actions:

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,7 +6,9 @@ from models.state import State
 from models.city import City
 from models import storage
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
 def get_cities(state_id):
     """Retrieves the list of all City objects of a State."""
     state = storage.get(State, state_id)
@@ -15,6 +17,7 @@ def get_cities(state_id):
     city_list = [city.to_dict() for city in state.city_list]
     return jsonify(city_list)
 
+
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     """Retrieves a City object."""
@@ -22,6 +25,7 @@ def get_city(city_id):
     if not city:
         return abort(404)
     return jsonify(city.to_dict())
+
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
@@ -33,7 +37,9 @@ def delete_city(city_id):
     storage.save()
     return jsonify({}), 200
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     """Creates a City in a State."""
     if request.content_type != 'application/json':
@@ -51,6 +57,7 @@ def create_city(state_id):
     storage.new(new_city)
     storage.save()
     return jsonify(new_city.to_dict()), 201
+
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
