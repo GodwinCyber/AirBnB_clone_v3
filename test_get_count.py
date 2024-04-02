cdfa4471666d4a5d85d3b7f3da3f23770cd77c6d
GodwinCyber <godwinchuks032@gmail.com>
2024-04-02 16:49:17 -0700

    updated the db and basemodel test

--- test_get_count.py
+++ test_get_count.py
@@ -12,5 +12,5 @@ first_state_id = list(storage.all(State).values())[0].id
 print("First state: {}".format(storage.get(State, first_state_id)))
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     unittest.main()
